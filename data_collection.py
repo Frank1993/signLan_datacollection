bd7ff7f2214a5c446bce0656a7bea8a59965f2a3
Frank Hu <hupengcheng1993@126.com>
2016-12-16 21:11:40 +0800

    update data_collection.py: add serialize and deserialize function of the frame

--- data_collection.py
+++ data_collection.py
@@ -1,11 +1,90 @@
-import ctypes
+import Leap
+import sys
+import ctypes,numpy
 
-frame = controller.frame()
+from PIL import Image
+import struct
 
-serialized_tuple = frame.serialize
-serialized_data = serialized_tuple[0]
-serialized_length = serialized_tuple[1]
-data_address = serialized_data.cast().__long__()
-buffer = (ctypes.c_ubyte * serialized_length).from_address(data_address)
-with open(os.path.realpath('frame.data'), 'wb') as data_file:
-    data_file.write(buffer)
+class DataPersistentListener(Leap.Listener):
+	def __init__(self,serialize_file):
+		super.__init__()
+		self.serialize_stream = open(serialize_file,'wb')
+
+    def on_connect(self, controller):
+        print "Connected"
+
+
+    def on_frame(self, controller):
+    	# get a frame
+        frame = controller.frame()
+        if not frame.images.is_empty:
+      		
+      		# get the raw data in the frame
+          	serialized_tuple = frame.serialize
+
+          	data = serialized_tuple[0]
+          	size = serialized_tuple[1]
+
+          	# write the length of the frame data, but we use 4 byte to represent the length with "int"
+
+          	self.serialize_stream.write(struct.pack("i",size))
+
+          	data_address = data.cast().__long__()
+
+          	data_buffer  = (ctypes.c_ubyte * size).from_address(data_address)
+
+          	self.serialize_stream.write(data_buffer)
+
+
+
+def Deserialization(object):
+	def __init__(self,serialized_stream):
+		self.frames = self.deserialize_frame(serialized_stream)
+
+
+	def deserialize_frame(serialized_stream):
+		frames = []
+		with open(serialized_stream,"rb") as frame_file:
+			next_block_size = frame_file.read(4)
+
+			while  next_block_size:
+				size = struct.unpack("i",next_block_size)[0]
+				data = frame_file.read(size)
+				leap_byte_array = Leap.byte_array(size)
+
+				address = leap_byte_array.cast().__long__()
+
+				ctypes.memmove(address,data,size)
+
+				frame = Leap.Frame()
+
+				frame.deserialize((leap_byte_array,size))
+				frames.append(frame)
+
+				next_block_size = frame_file.read(4)
+
+
+def main():
+    # Create a sample listener and controller
+
+    listener = SampleListener(sys.argv[0])
+    controller = Leap.Controller()
+    controller.set_policy(Leap.Controller.POLICY_IMAGES)
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Remove the sample listener when done
+        listener.serialize_stream.close()
+        controller.remove_listener(listener)
+
+if __name__ == "__main__":
+    main()
